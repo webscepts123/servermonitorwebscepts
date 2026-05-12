a9bbfa592366d40cf643c07e8f2f8e3646c1d4a9
Webscept Solutions <websceptsolution@gmail.com>
2026-05-13 02:02:47 +0530

    passkey

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,31 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passkeys\Contracts\PasskeyUser;
+use Laravel\Passkeys\PasskeyAuthenticatable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements PasskeyUser
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, PasskeyAuthenticatable;
+
+    /**
+     * Get the passkey display name.
+     */
+    public function getPasskeyDisplayName(): string
+    {
+        return $this->name ?: $this->email ?: 'User';
+    }
+
+    /**
+     * Get the passkey username.
+     */
+    public function getPasskeyUsername(): string
+    {
+        return $this->email ?: (string) $this->getKey();
+    }
 
     /**
      * Get the attributes that should be cast.
