63a0a358b3667dc3ac31970a2e2bfb649a7d545a
Webscept Solutions <websceptsolution@gmail.com>
2026-05-13 01:59:32 +0530

    layoutapp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('page-title', 'Dashboard') - Webscepts Monitoring</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @vite(['resources/js/app.js'])
 
     <script src="https://cdn.tailwindcss.com"></script>
 
